7b16e386357ab3261460f8c16717857110a9fcc9
ketrika07 <116058601+ketrika07@users.noreply.github.com>
2024-06-28 16:19:41 +0300

    maj

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,11 @@
   <meta name="author" content="">
   <title>CONSTRUCTION - BTP</title>
   <!-- Custom fonts for this template-->
-  <link href="https://evalbtp-production.up.railway.app/admin_assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   <!-- Custom styles for this template-->
-  <link href="https://evalbtp-production.up.railway.app/admin_assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
 <body id="page-top">
   <!-- Page Wrapper -->
@@ -67,13 +67,13 @@
   </a>
   
   <!-- Bootstrap core JavaScript-->
-  <script src="https://evalbtp-production.up.railway.app/admin_assets/vendor/jquery/jquery.min.js"></script>
-  <script src="https://evalbtp-production.up.railway.app/admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('admin_assets/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <!-- Core plugin JavaScript-->
-  <script src="https://evalbtp-production.up.railway.app/admin_assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="{{ asset('admin_assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
   <!-- Custom scripts for all pages-->
-  <script src="https://evalbtp-production.up.railway.app/admin_assets/js/sb-admin-2.min.js"></script>
+  <script src="{{ asset('admin_assets/js/sb-admin-2.min.js') }}"></script>
   <!-- Page level plugins -->
-  <script src="https://evalbtp-production.up.railway.app/admin_assets/vendor/chart.js/Chart.min.js"></script>
+  <script src="{{ asset('admin_assets/vendor/chart.js/Chart.min.js') }}"></script>
 </body>
 </html>
