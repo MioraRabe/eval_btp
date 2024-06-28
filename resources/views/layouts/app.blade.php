2a014547248773470a7055580afe6ab7c237425f
ketrika07 <116058601+ketrika07@users.noreply.github.com>
2024-06-28 15:19:56 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,11 @@
   <meta name="author" content="">
   <title>CONSTRUCTION - BTP</title>
   <!-- Custom fonts for this template-->
-  <link href="admin_assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  {{--  <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">  --}}
+  <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   <!-- Custom styles for this template-->
-  <link href="admin_assets/css/sb-admin-2.min.css" rel="stylesheet">
-  {{--  <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">  --}}
+  <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
 <body id="page-top">
   <!-- Page Wrapper -->
