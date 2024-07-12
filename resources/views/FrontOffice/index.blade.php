bfd74bb7d0cde95cdaf0bbaa98b7371e6b48885b
ketrika07 <116058601+ketrika07@users.noreply.github.com>
2024-07-12 12:55:09 +0300

    add tag google analytics

--- resources/views/FrontOffice/index.blade.php
+++ resources/views/FrontOffice/index.blade.php
@@ -35,6 +35,18 @@
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-T5ZHDVNX');</script>
     <!-- End Google Tag Manager -->
+
+    <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-65KF96TJSN"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-65KF96TJSN');
+        </script>
+
+        
   </head>
   <body>
             <!-- Google Tag Manager (noscript) -->
