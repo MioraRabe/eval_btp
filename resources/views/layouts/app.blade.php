8f75d4c7b7e5fbdc1cd1103a60d32eae8960b224
ketrika07 <116058601+ketrika07@users.noreply.github.com>
2024-07-12 20:13:41 +0300

    fix error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,14 +23,14 @@
 
 
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-65KF96TJSN"></script>
+        {{--  <script async src="https://www.googletagmanager.com/gtag/js?id=G-65KF96TJSN"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'G-65KF96TJSN');
-        </script>
+        </script>  --}}
 
 
 
