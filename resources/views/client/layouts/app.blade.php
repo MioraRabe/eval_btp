a2365f1d8e652d61081760444a6df423429d9def
ketrika07 <116058601+ketrika07@users.noreply.github.com>
2024-07-13 18:19:03 +0300

    add google tag and analytics in layouts(clients)

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -10,62 +10,87 @@
   <!-- Custom fonts for this template-->
   <link href="https://evalbtp-production.up.railway.app/admin_assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  
+
   <!-- Custom styles for this template-->
   <link href="https://evalbtp-production.up.railway.app/admin_assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T5ZHDVNX');</script>
+        <!-- End Google Tag Manager -->
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-65KF96TJSN"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-65KF96TJSN');
+        </script>
+
 </head>
 <body id="page-top">
+
+    <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5ZHDVNX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+
   <!-- Page Wrapper -->
   <div id="wrapper">
-  
+
     <!-- Sidebar -->
     @include('client.layouts.sidebar')
     <!-- End of Sidebar -->
-  
+
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
-  
+
       <!-- Main Content -->
       <div id="content">
-  
+
         <!-- Topbar -->
         @include('client.layouts.navbar')
         <!-- End of Topbar -->
-  
+
         <!-- Begin Page Content -->
         <div class="container-fluid">
-  
+
           <!-- Page Heading -->
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">@yield('title')</h1>
           </div>
-  
+
           @yield('contents')
-  
+
           <!-- Content Row -->
-  
-  
+
+
         </div>
         <!-- /.container-fluid -->
-  
+
       </div>
       <!-- End of Main Content -->
-  
+
       <!-- Footer -->
       @include('client.layouts.footer')
       <!-- End of Footer -->
-  
+
     </div>
     <!-- End of Content Wrapper -->
-  
+
   </div>
   <!-- End of Page Wrapper -->
-  
+
   <!-- Scroll to Top Button-->
   <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
   </a>
-  
+
   <!-- Bootstrap core JavaScript-->
   <script src="https://evalbtp-production.up.railway.app/admin_assets/vendor/jquery/jquery.min.js"></script>
   <script src="https://evalbtp-production.up.railway.app/admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
