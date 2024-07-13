f9ed328c40123d635109e8988f7973b8acac3111
ketrika07 <116058601+ketrika07@users.noreply.github.com>
2024-07-13 11:00:04 +0300

    change

--- resources/views/FrontOffice/index.blade.php
+++ resources/views/FrontOffice/index.blade.php
@@ -83,7 +83,7 @@
     <!-- END nav -->
 
     <section class="home-slider owl-carousel">
-		<div class="slider-item" style="background-image: url(https://evalbtp-production.up.railway.app/clients_assets/images/btp1.jpg);" loading="lazy"></div>
+      <div class="slider-item" style="background-image: url(https://evalbtp-production.up.railway.app/clients_assets/images/btp1.jpg);">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
